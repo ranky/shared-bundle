f0e3afed1657a9de1a2dd682c5924db12832e251
chiqui3d <nerjacarloscampos@gmail.com>
2023-01-30 22:12:20 +0100

    test(fix): comment unnecessary behat service import

--- tests/config/services.php
+++ tests/config/services.php
@@ -17,8 +17,8 @@ return static function (ContainerConfigurator $configurator): void {
     $configurator->parameters()->set('site_url', '%env(resolve:SITE_URL)%');
 
   // For KernelInterface in AbstractApiContext
-  $services
-        ->load('Ranky\\SharedBundle\\Tests\\Presentation\\Behat\\', '../src/Presentation/Behat/*');
+  /*$services
+        ->load('Ranky\\SharedBundle\\Tests\\Presentation\\Behat\\', '../src/Presentation/Behat/*');*/
 
     $services->set(SiteUrlResolver::class);
     $services->alias(SiteUrlResolverInterface::class, SiteUrlResolver::class)->public();
