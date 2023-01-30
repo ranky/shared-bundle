2a498475db6aaf40338e74eaa6385c7a9817da30
chiqui3d <nerjacarloscampos@gmail.com>
2023-01-30 16:07:47 +0100

    ci(fix): changed mb_str_functions rule to false to avoid requiring the mbstring extension.

--- tools/php-cs-fixer/.php-cs-fixer.dist.php
+++ tools/php-cs-fixer/.php-cs-fixer.dist.php
@@ -11,49 +11,49 @@ use PhpCsFixer\Finder;
  */
 $fileHeaderComment = <<<COMMENT
     This file is part of the Ranky Media Bundle package.
-    (c) Jose Carlos Campos <josecarlos@clicksolution.es>
+    (c) Jose Carlos Campos <nerjacarloscampos@gmail.com>
     For the full copyright and license information, please view the LICENSE
     file that was distributed with this source code.
     COMMENT;
 
 $rules = [
-    '@Symfony:risky' => true,
-    '@PHPUnit84Migration:risky' => true,
-    '@PHP80Migration:risky' => true,
-    '@PHP81Migration' => true,
-    'header_comment' => ['header' => '', 'location' => 'after_declare_strict', 'separate' => 'both'],
-    'linebreak_after_opening_tag' => true,
-    'combine_consecutive_issets' => true,
-    'combine_consecutive_unsets' => true,
-    'declare_strict_types' => true,
-    'single_quote' => true,
-    'declare_parentheses' => true,
-    'mb_str_functions' => false,
-    'modernize_strpos' => true,
-    'no_php4_constructor' => true,
-    'no_unused_imports' => true,
-    'no_unreachable_default_argument_value' => true,
-    'no_useless_else' => true,
-    'no_useless_return' => true,
-    'native_function_invocation' => false,
-    'native_constant_invocation' => false,
-    'date_time_create_from_format_call' => true,
-    'php_unit_strict' => false,
-    'no_alias_functions' => true,
+    '@Symfony:risky'                              => true,
+    '@PHPUnit84Migration:risky'                   => true,
+    '@PHP80Migration:risky'                       => true,
+    '@PHP81Migration'                             => true,
+    'header_comment'                              => ['header' => '', 'separate' => 'both'],
+    'linebreak_after_opening_tag'                 => true,
+    'combine_consecutive_issets'                  => true,
+    'combine_consecutive_unsets'                  => true,
+    'declare_strict_types'                        => true,
+    'single_quote'                                => true,
+    'declare_parentheses'                         => true,
+    'mb_str_functions'                            => false,
+    'modernize_strpos'                            => true,
+    'no_php4_constructor'                         => true,
+    'no_unused_imports'                           => true,
+    'no_unreachable_default_argument_value'       => true,
+    'no_useless_else'                             => true,
+    'no_useless_return'                           => true,
+    'native_function_invocation'                  => false,
+    'native_constant_invocation'                  => false,
+    'date_time_create_from_format_call'           => true,
+    'php_unit_strict'                             => false,
+    'no_alias_functions'                          => true,
     'no_multiline_whitespace_around_double_arrow' => true,
-    'phpdoc_order' => true,
-    'strict_comparison' => true,
-    'strict_param' => true,
-    'line_ending' => true,
+    'phpdoc_order'                                => true,
+    'strict_comparison'                           => true,
+    'strict_param'                                => true,
+    'line_ending'                                 => true,
 ];
 
-$config  = new PhpCsFixer\Config();
-$rootDir = \dirname(__DIR__, 2);
+$config         = new PhpCsFixer\Config();
+$rootDir        = \dirname(__DIR__, 2);
 
 return $config
     ->setRules($rules)
     ->setUsingCache(true)
-    ->setCacheFile(__DIR__.'/var/php-cs-fixer.cache')
+    ->setCacheFile(__DIR__.'/php-cs-fixer.cache')
     ->setRiskyAllowed(true)
     ->setFinder(
         Finder::create()->in([
