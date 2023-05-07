25f7af0f3c97dc3aedbc5395f026e2317c61eb89
chiqui3d <nerjacarloscampos@gmail.com>
2023-05-07 14:10:46 +0200

    style: apply PHP CS Fixer

--- tests/src/Common/ClassHelperTest.php
+++ tests/src/Common/ClassHelperTest.php
@@ -42,8 +42,8 @@ class ClassHelperTest extends TestCase
      *
      * @param object $input
      * @param array<string, mixed> $expected
-     * @throws \ReflectionException
      * @return void
+     * @throws \ReflectionException
      */
     public function testItShouldConvertObjectToArray(object $input, array $expected): void
     {
@@ -58,7 +58,17 @@ class ClassHelperTest extends TestCase
         $page = PageFactory::create(1, 'this is a title', 'this is a description');
 
         return [
-            [$page, ['id' => 1, 'title' => 'this is a title', 'description' => 'this is a description']],
+            [
+                $page,
+                [
+                    'id' => 1,
+                    'title' => 'this is a title',
+                    'description' => 'this is a description',
+                    'pageUlid' => null,
+                    'pageUuid' => null,
+                    'pageCollection' => null,
+                ],
+            ],
         ];
     }
 
