8c8b7445cee1535d4ac2055f5621ffb6d8980ce8
chiqui3d <nerjacarloscampos@gmail.com>
2023-05-07 14:11:08 +0200

    fix: correct namespace

--- tests/src/Infrastructure/Persistence/Dbal/UserIdentifierTypeTest.php
+++ tests/src/Infrastructure/Persistence/Dbal/UserIdentifierTypeTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 
-namespace Ranky\SharedBundle\Infrastructure\Persistence\Dbal;
+namespace Ranky\SharedBundle\Tests\Infrastructure\Persistence\Dbal;
 
 use Doctrine\DBAL\Platforms\MySQLPlatform;
 use PHPUnit\Framework\TestCase;
