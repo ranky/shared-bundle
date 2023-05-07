09ba8534eb04553c3deb571b44a99ad47beeec68
chiqui3d <nerjacarloscampos@gmail.com>
2023-05-07 14:16:31 +0200

    feat: update services.php in tests to configure new services

--- tests/config/services.php
+++ tests/config/services.php
@@ -5,21 +5,41 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 
 
-use Ranky\SharedBundle\Domain\Site\SiteUrlResolverInterface;
-use Ranky\SharedBundle\Infrastructure\Site\SiteUrlResolver;
+use Ranky\SharedBundle\Domain\Site\SiteUrlResolver;
+use Ranky\SharedBundle\Infrastructure\Persistence\Orm\UidMapperPlatform;
+use Ranky\SharedBundle\Infrastructure\Site\SymfonySiteUrlResolver;
+use Ranky\SharedBundle\Tests\Dummy\Page\Domain\PageRepository;
+use Ranky\SharedBundle\Tests\Dummy\Page\Infrastructure\Persistence\ORM\Repository\DoctrineOrmPageRepository;
+use Ranky\SharedBundle\Tests\Dummy\Page\Presentation\PageApiController;
 
 return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services()
         ->defaults()
+        ->public()
         ->autoconfigure() // Automatically registers your services as commands, event subscribers, etc
         ->autowire(); // Automatically injects dependencies in your services
 
     $configurator->parameters()->set('site_url', '%env(resolve:SITE_URL)%');
 
-  // For KernelInterface in AbstractApiContext
-  /*$services
-        ->load('Ranky\\SharedBundle\\Tests\\Presentation\\Behat\\', '../src/Presentation/Behat/*');*/
+    $services
+        ->load('Ranky\\SharedBundle\\Tests\\', '../src/*')
+        ->exclude([
+            '../src/**/*Interface.php',
+            '../src/**/*Constraint.php',
+            '../src/**/*Request.php',
+            '../src/**/*Response.php',
+            '../src/**/Domain',
+            '../src/Common',
+        ]);
+    $services
+        ->set(PageApiController::class)
+        ->tag('controller.service_arguments');
 
-    $services->set(SiteUrlResolver::class);
-    $services->alias(SiteUrlResolverInterface::class, SiteUrlResolver::class)->public();
+    $services->set(UidMapperPlatform::class);
+    $services->set(SymfonySiteUrlResolver::class);
+    $services->alias(SiteUrlResolver::class, SymfonySiteUrlResolver::class)->public();
+
+    $services->set(DoctrineOrmPageRepository::class);
+    $services->alias(PageRepository::class, DoctrineOrmPageRepository::class)->public();
+
 };
